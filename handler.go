e76599e9266edd087c357c5c4aec5552b7a07696
Qro <core.editor>
2023-09-22 21:21:19 +0900

    行き詰った

---
+++ handler.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type City struct {
+	ID          int            `json:"id,omitempty"  db:"ID"`
+	Name        sql.NullString `json:"name,omitempty"  db:"Name"`
+	CountryCode sql.NullString `json:"countryCode,omitempty"  db:"CountryCode"`
+	District    sql.NullString `json:"district,omitempty"  db:"District"`
+	Population  sql.NullInt64  `json:"population,omitempty"  db:"Population"`
+}
+
+func getCityInfoHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	fmt.Println(cityName)
+
+	var city City
+	db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName)
+	if !city.Name.Valid {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	return c.JSON(http.StatusOK, city)
+}
+
+func postCityHandler(c echo.Context) error {
+	var city City
+	err := c.Bind(&city)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "bad request body")
+	}
+
+	result, err := db.Exec("INSERT INTO city (Name, CountryCode, District, Population) VALUES (?, ?, ?, ?)", city.Name, city.CountryCode, city.District, city.Population)
+	if err != nil {
+		log.Printf("failed to insert city data: %s\n", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		fmt.Printf("failed to get last insert id: %s\n", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	city.ID = int(id)
+
+	return c.JSON(http.StatusCreated, city)
+}
+
+type LoginRequestBody struct {
+	Username string `json:"username,omitempty" form:"username"`
+	Password string `json:"password,omitempty" form:"password"`
+}
+
+func signUpHandler(c echo.Context) error {
+	// リクエストを受け取り、reqに格納する
+	req := LoginRequestBody{}
+	c.Bind(&req)
+
+	// バリデーションする(PasswordかUsernameが空文字列の場合は400 BadRequestを返す)
+	if req.Password == "" || req.Username == "" {
+		return c.String(http.StatusBadRequest, "Username or Password is empty")
+	}
+
+	// 登録しようとしているユーザーが既にデータベース内に存在するかチェック
+	var count int
+	err := db.Get(&count, "SELECT COUNT(*) FROM users WHERE Username=?", req.Username)
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	// 存在したら409 Conflictを返す
+	if count > 0 {
+		return c.String(http.StatusConflict, "Username is already used")
+	}
+
+	// パスワードをハッシュ化する
+	pw := req.Password + salt
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
+	// ハッシュ化に失敗したら500 InternalServerErrorを返す
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	// ユーザーを登録する
+	_, err = db.Exec("INSERT INTO users (Username, HashedPass) VALUES (?, ?)", req.Username, hashedPass)
+	// 登録に失敗したら500 InternalServerErrorを返す
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	// 登録に成功したら201 Createdを返す
+	return c.NoContent(http.StatusCreated)
+}
+
+type User struct {
+	Username   string `json:"username,omitempty" form:"Username"`
+	HashedPass string `json:"-" form:"HashedPass"`
+}
+
+func loginHandler(c echo.Context) error {
+	var req LoginRequestBody
+	c.Bind(&req)
+
+	if req.Password == "" || req.Username == "" {
+		return c.String(http.StatusBadRequest, "Username or Password is empty")
+	}
+	user := User{}
+	err := db.Get(&user, "SELECT Username, HashedPass FROM users WHERE Username=?", req.Username)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return c.NoContent(http.StatusUnauthorized)
+		} else {
+			log.Println(err)
+			log.Println(1)
+			return c.NoContent(http.StatusInternalServerError)
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.HashedPass), []byte(req.Password+salt))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return c.NoContent(http.StatusUnauthorized)
+		} else {
+			log.Println(2)
+			return c.NoContent(http.StatusInternalServerError)
+		}
+	}
+
+	sess, err := session.Get("sessions", c)
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "something wrong in getting session")
+	}
+	sess.Values["userName"] = req.Username
+	sess.Save(c.Request(), c.Response())
+
+	return c.NoContent(http.StatusOK)
+
+}
+
+func userAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		sess, err := session.Get("sessions", c)
+		if err != nil {
+			fmt.Println(err)
+			return c.String(http.StatusInternalServerError, "something wrong in getting session")
+		}
+		if sess.Values["userName"] == nil {
+			return c.String(http.StatusUnauthorized, "please login")
+		}
+		c.Set("userName", sess.Values["userName"].(string))
+
+		return next(c)
+	}
+}
+
+type Me struct {
+	Username string `json:"username,omitempty"  db:"username"`
+}
+
+func getWhoAmIHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, Me{
+		Username: c.Get("userName").(string),
+	})
+}
--- main.go
+++ main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/srinathgs/mysqlstore"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var (
+	db   *sqlx.DB
+	salt = os.Getenv("HASH_SALT")
+)
 
 func main() {
-	fmt.Println("なろう講習会へようこそ！")
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf := mysql.Config{
+		User:      "root",
+		Passwd:    "password",
+		Net:       "tcp",
+		Addr:      "localhost:3306",
+		DBName:    "world",
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+
+	_db, err := sqlx.Open("mysql", conf.FormatDSN())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db = _db
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (Username VARCHAR(255) PRIMARY KEY, HashedPass VARCHAR(255))")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	store, err := mysqlstore.NewMySQLStoreFromConnection(db.DB, "sessions", "/", 60*60*24*14, []byte("secret-token"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(session.Middleware(store))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e.POST("/signup", signUpHandler)
+	e.POST("/login", loginHandler)
+	withAuth := e.Group("")
+	withAuth.Use(userAuthMiddleware)
+	withAuth.GET("/cities/:cityName", getCityInfoHandler)
+	withAuth.POST("/cities", postCityHandler)
+	withAuth.GET("/whoami", getWhoAmIHandler)
+	e.Start(":8080")
 }
